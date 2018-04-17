949e8c6fff4af5ddc8e8f62142c55a04f35f7842
Patryk Kacperski <221599@edu.p.lodz.pl>
2018-04-17 12:56:29 +0200

    Created spring boot backend project

---
+++ masi-STP-backend/src/main/java/pl/lodz/p/it/masi/stp/chatbot/masiSTPbackend/MasiStpBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.it.masi.stp.chatbot.masiSTPbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasiStpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasiStpBackendApplication.class, args);
+	}
+}
