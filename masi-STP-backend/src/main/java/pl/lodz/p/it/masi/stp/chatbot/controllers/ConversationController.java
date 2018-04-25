bf2518711d25043014999636b164c63d499e6fbb
Michał Ladra <221617@edu.p.lodz.pl>
2018-04-25 08:04:04 +0200

    [MASSP-29]:   - poprawienie wysokości dolnego paska do wpisywania tekstu. #time 1m [MASSP-30]   - dodanie adnotacji `@CrossOrigin` do controller'a. #time 1m

--- masi-STP-backend/src/main/java/pl/lodz/p/it/masi/stp/chatbot/controllers/ConversationController.java
+++ masi-STP-backend/src/main/java/pl/lodz/p/it/masi/stp/chatbot/controllers/ConversationController.java
@@ -1,13 +1,15 @@
 package pl.lodz.p.it.masi.stp.chatbot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import pl.lodz.p.it.masi.stp.chatbot.entities.MessageDto;
 import pl.lodz.p.it.masi.stp.chatbot.services.ConversationService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/chat")
 public class ConversationController {
